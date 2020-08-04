0e35d8bbe349983469ab3733626d0a37137e9bac
Adam <adam.kowalczuk@poczta.fm>
2020-08-04 10:08:19 +0200

    Dodane wyświetlanie produktów

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={ProductList} />
           <Route path="/details" component={Details} />
-          <Route path="/cart" component={Cart}></Route>
-          <Route component={Default}></Route>
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
         </Switch>
       </React.Fragment>
     );
